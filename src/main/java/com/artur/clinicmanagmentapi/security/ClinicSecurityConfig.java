c49ab0708647415793ec3312d99ec96207fa4cce
artur <itoskaletto@gmail.com>
2026-04-22 18:47:39 +0200

    feat: secure REST endpoints based on roles and disable CSRF

--- src/main/java/com/artur/clinicmanagmentapi/security/ClinicSecurityConfig.java
+++ src/main/java/com/artur/clinicmanagmentapi/security/ClinicSecurityConfig.java
@@ -3,9 +3,13 @@ package com.artur.clinicmanagmentapi.security;
 import jdk.jshell.spi.ExecutionControl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.HttpServerErrorException;
 
 @Configuration
@@ -34,4 +38,27 @@ public class ClinicSecurityConfig {
 
         return new InMemoryUserDetailsManager(mary, john, susan);
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/patients").hasRole("NURSE")
+                        .requestMatchers(HttpMethod.GET, "/api/patients/**").hasRole("NURSE")
+                        .requestMatchers(HttpMethod.PUT, "/api/patients").hasRole("DOCTOR")
+                        .requestMatchers(HttpMethod.POST, "/api/patients").hasRole("DOCTOR")
+                        .requestMatchers(HttpMethod.PATCH, "/api/patients/**").hasRole("DOCTOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/patients/**").hasRole("ADMIN")
+        );
+
+        // use http basic auth
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable csrf
+        http.csrf(csrf-> csrf.disable());
+
+        return http.build();
+    }
+
 }
